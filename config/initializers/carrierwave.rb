9fdcdfcccc383499bbc551cbce6b00e7a684bf68
Naoki <lmn.bsk210@gmail.com>
2020-04-11 12:40:32 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'exhoop'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/exhoop'
+  config.asset_host = 'https://exhoop.s3-ap-northeast-1.amazonaws.com'
 end
