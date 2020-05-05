9ca447c0abca3b51efd2ec65f0cc93c1c03e0b8b
Naoki <lmn.bsk210@gmail.com>
2020-05-05 13:08:28 +0900

    Update video_play.js

--- app/assets/javascripts/video_play.js
+++ app/assets/javascripts/video_play.js
@@ -1,11 +1,11 @@
 $(document).on({
   mouseenter:function(){
     $(this).addClass('on');
-    $(this).find('.video')[0].currentTime = $(this).find('.video')[0].initialTime || 0;
     $(this).find('.video')[0].play(); 
   },
   mouseleave:function(){
     $(this).removeClass('on');
+    $(this).find('.video')[0].currentTime = $(this).find('.video')[0].initialTime || 0;
     $(this).find('.video')[0].pause();
   }
 },'.content__box__video');
