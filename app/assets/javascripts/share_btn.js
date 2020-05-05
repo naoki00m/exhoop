1f32af282438872fa856dd4f776503bf2a074a36
Naoki <lmn.bsk210@gmail.com>
2020-05-05 19:13:03 +0900

    fix/sns-share
    ・SNSへの共有ボタンのJSを調整

--- app/assets/javascripts/share_btn.js
+++ app/assets/javascripts/share_btn.js
@@ -3,4 +3,9 @@ $(function() {
   $(".content__show__box__bottom__icons__share").on("click", function() {
     $(".content__show__box__bottom__icons__share__area").toggle();
   });
+  $(document).on("click touchend", function(){
+    if (!$(event.target).closest(".content__show__box__bottom__icons__share").length) {
+      $(".content__show__box__bottom__icons__share__area").hide();
+    }
+  });
 });
