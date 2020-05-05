586332cea878be8d773b5838832f43e15b0eeec5
Naoki <lmn.bsk210@gmail.com>
2020-05-05 15:05:30 +0900

    feature/share
    ・Twitter、LINEへの動画共有
    ・共有ボタンの作成

---
+++ app/assets/javascripts/share_btn.js
@@ -0,0 +1,6 @@
+$(function() {
+  $(".content__show__box__bottom__icons__share__area").hide();
+  $(".content__show__box__bottom__icons__share").on("click", function() {
+    $(".content__show__box__bottom__icons__share__area").toggle();
+  });
+});
